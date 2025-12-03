691472f6f77003b35bb81469351a9f38db474316
Kenneth Murerwa <xwaxes@gmail.com>
2025-12-03 10:41:21 +0300

    Initial commit

---
+++ day1/task1.go
@@ -0,0 +1,7 @@
+package day1
+
+import "fmt"
+
+func Task1() {
+	fmt.Println("Hello, Task 1!")
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"aoc2025/day1"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	day1.Task1()
+}
